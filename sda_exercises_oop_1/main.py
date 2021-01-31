758c28d250c8c389ce4ff676ab764364cf4f3115
89adamstefan <89adamstefan@gmail.com>
2021-01-31 13:01:01 +0100

    Created cat def

--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -1,14 +1,11 @@
-def main():
-    print("Madzix")
+from sda_exercises_oop_1.cat import Cat
 
+
+def main():
+    cat_object = Cat("Mruczek")
+    sound = cat_object.make_sound()
+    print(sound)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
