96b1153dd766e41f21c4fa3d5aafd88e8aa34a19
89adamstefan <89adamstefan@gmail.com>
2021-02-06 15:02:34 +0100

    Add second test

--- figures/tests.py
+++ figures/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from figures.figures_entities import Figure, Circle, Triangle, Rectangle
+from figures.figures_entities import Figure, Circle, Triangle, Rectangle, count_area_func
 
 
 def test_count_area():
@@ -11,3 +11,14 @@ def test_count_area():
     result = Figure.count_area([circle1, triangle1, rectangle1])
     #  then
     assert result == 8.14
+
+def test_check_area():
+    #  given
+    circle1 = Circle(1)
+    triangle1 = Triangle(2, 1)
+    rectangle1 = Rectangle(2, 2)
+    area = count_area_func(triangle1, circle1, rectangle1)
+    #  when
+    result = Figure.check_area(area, [circle1, triangle1, rectangle1])
+    #  then
+    assert result <= 15
