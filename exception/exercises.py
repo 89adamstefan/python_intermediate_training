c9fcebcb1fff9fbdd5cea9c44c14cd9228b9b161
89adamstefan <89adamstefan@gmail.com>
2021-02-07 10:59:23 +0100

    add ZeroDivisorError

--- exception/exercises.py
+++ exception/exercises.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def case_1():
     list_of_numbers = [1, 5, 8, 10, 21]
     print("Case_1 before")
@@ -19,3 +22,14 @@ def case_2(name: str):
     if len(name) <= 0:
         raise ValueError('String is empty!')
     print(f'Given name is: {name}')
+
+def case_3(number: int, divisor: int) -> float:
+    result = 0
+    try:
+        result = number / divisor
+    except ZeroDivisionError as zde:
+        print("The divisor value cannot be zero")
+        result = sys.float_info.max
+        # result = float(sys.maxsize)
+    return result
+
--- exception/main.py
+++ exception/main.py
@@ -1,12 +1,17 @@
-from exception.exercises import case_1, case_2
+from exception.exercises import case_1, case_2, case_3
 
 
 def main():
     print("Start app")
-    try:
-        case_2("")
-    except ValueError as ve:
-        print(f'ValueError return {ve.args}')
+    # try:
+    #     case_2("")
+    # except ValueError as ve:
+    #     print(f'ValueError return {ve.args}')
+
+    result = case_3(10, 0)
+    print(result)
+
+
     print("Finish")
 
 
