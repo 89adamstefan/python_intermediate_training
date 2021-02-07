d41b2d559415171ab02369925c90f5c986f47e36
89adamstefan <89adamstefan@gmail.com>
2021-02-07 13:15:43 +0100

    Add pickle reader

--- serialize/main.py
+++ serialize/main.py
@@ -1,9 +1,12 @@
-from serialize.pickle_training import pickle_write
+from serialize.pickle_training import pickle_write, pickle_read
 
 
 def main():
     abc = ['milk', 'bread', 'butter']
     pickle_write(abc)
+
+    list_of_string = pickle_read()
+    print(list_of_string)
 
 
 if __name__ == '__main__':
--- serialize/pickle_training.py
+++ serialize/pickle_training.py
@@ -2,9 +2,22 @@ import pickle
 
 
 def pickle_write(items: list):
+    print("Pickle write start!")
     try:
         with open('./data.pickle', 'wb') as fd:
             pickle.dump(items, fd)
             print(pickle.dumps(items))
     except (IOError, Exception) as e:
         print(f'Exception while writing pickle format, info {e.args}')
+    print("Finish!")
+def pickle_read():
+    print("Pickle read start!")
+    string_list = []
+    try:
+        with open('./data.pickle', 'rb') as fd:
+            string_list = pickle.load(fd)
+    except (IOError, Exception) as e:
+        print(f'Exception while reading pickle format, info {e.args}')
+    print("Finish!")
+    return string_list
+
