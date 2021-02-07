1bcff3ca652d6822c4a9d4b9c99bd00486a47298
89adamstefan <89adamstefan@gmail.com>
2021-02-07 10:10:22 +0100

    New exceptions added mate

--- exception/exercises.py
+++ exception/exercises.py
@@ -1,5 +1,14 @@
 def case_1():
     list_of_numbers = [1, 5, 8, 10, 21]
     print("Case_1 before")
-    result = list_of_numbers[5]
+    try:
+        result = list_of_numbers[6]
+    except IndexError as ie:
+        print(f'Exception cought {ie.args}')
+    except Exception as exp:
+        print(f'Exception cought {exp.args}')
     print("Case_1 after")
+    try:
+        result = list_of_numbers[6]
+    except (IndexError, Exception) as e:
+        print(f'Exception cought by tuple {e.args}')
