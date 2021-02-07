3b287b59b91259458768472579908a9b54af69e8
89adamstefan <89adamstefan@gmail.com>
2021-02-07 10:12:13 +0100

    small change in file exercises

--- exception/exercises.py
+++ exception/exercises.py
@@ -1,6 +1,7 @@
 def case_1():
     list_of_numbers = [1, 5, 8, 10, 21]
     print("Case_1 before")
+    #  first example
     try:
         result = list_of_numbers[6]
     except IndexError as ie:
@@ -8,6 +9,7 @@ def case_1():
     except Exception as exp:
         print(f'Exception cought {exp.args}')
     print("Case_1 after")
+    #  second example
     try:
         result = list_of_numbers[6]
     except (IndexError, Exception) as e:
