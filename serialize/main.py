a2b4d9f615c0a8400ad03d60f0d93e1693b244bb
89adamstefan <89adamstefan@gmail.com>
2021-02-20 10:20:33 +0100

    Add next method mate

--- serialize/main.py
+++ serialize/main.py
@@ -1,6 +1,9 @@
 from serialize.csv_training import csv_write, csv_read
+from serialize.human import Human, json_human_to_file
 from serialize.json_training import json_to_file, json_from_file
 from serialize.pickle_training import pickle_write, pickle_read
+
+
 
 
 def main():
@@ -23,8 +26,15 @@ def main():
 
     # JSON_TRAINING!
     # json_to_file()
-    print(json_from_file())
+    # print(json_from_file())
 
+    h1 = Human(30, "Jan", "Kowalski")
+    h2 = Human(25, "Anna", "Kowalska")
+    h3 = Human(40, "Tomasz", "Nowak")
+
+    humans = [h1, h2, h3]
+
+    json_human_to_file(humans)
 
 if __name__ == '__main__':
     main()
