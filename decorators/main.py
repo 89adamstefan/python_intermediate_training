b5f719c6e2480adc9322a10839e887ddb9be0781
89adamstefan <89adamstefan@gmail.com>
2021-02-20 12:40:36 +0100

    Add new wrapper

--- decorators/main.py
+++ decorators/main.py
@@ -1,23 +1,30 @@
+from decorators.case_2 import read_file
+
+
 def main():
-    a, b = print_helo_world(1, 2)
-    print(f'In main {a} {b}')
+#     a, b = print_helo_world(1, 2)
+#     print(f'In main {a} {b}')
+#
+#
+# def wrap_before_and_after(func):
+#     def wrapper(*args, **kwargs):
+#         print("Before")
+#         result = func(*args, **kwargs)
+#         print("After")
+#         return result
+#
+#     return wrapper
+#
+#
+# @wrap_before_and_after
+# def print_helo_world(a, b):
+#     print("Hello World!")
+#     print(f'{a, b}')
+#     return a, b
 
+# CASE_2!
+    read_file(file_path='./abc')
 
-def wrap_before_and_after(func):
-    def wrapper(*args, **kwargs):
-        print("Before")
-        result = func(*args, **kwargs)
-        print("After")
-        return result
-
-    return wrapper
-
-
-@wrap_before_and_after
-def print_helo_world(a, b):
-    print("Hello World!")
-    print(f'{a, b}')
-    return a, b
 
 
 if __name__ == '__main__':
