f901db1a1f8e8ae855e98c03f6825a29b5f45c5c
89adamstefan <89adamstefan@gmail.com>
2021-02-21 11:08:30 +0100

    Add ex2

--- context_manager/main.py
+++ context_manager/main.py
@@ -1,8 +1,10 @@
 from context_manager.case_1 import exercise_1
+from context_manager.case_2 import exercise_2
 
 
 def main():
-    exercise_1()
+    # exercise_1()
+    exercise_2()
 
 
 if __name__ == '__main__':
