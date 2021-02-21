7d74c73d9ead026e5b1fbd2464b0814ee3ea2055
89adamstefan <89adamstefan@gmail.com>
2021-02-21 09:49:37 +0100

    Add new exercises

--- generators/main.py
+++ generators/main.py
@@ -1,6 +1,8 @@
 from generators.case_1 import iterator_ex_1
 from generators.case_2 import iterator_ex_2
 from generators.case_3 import iterator_ex_3
+from generators.case_4 import exercise_4
+from generators.case_5 import reader_line_by_line
 
 
 def main():
@@ -14,6 +16,14 @@ def main():
     # CASE_3!
     print("Exercise 3")
     iterator_ex_3(1000)
+
+    # CASE_4!
+    print("Exercise 4")
+    exercise_4()
+
+    # CASE_5!
+    print("Exercise 5")
+    reader_line_by_line()
 
 
 if __name__ == '__main__':
